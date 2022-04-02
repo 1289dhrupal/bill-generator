5bf9c88fa0b4be6c514b3ed1eca7d65ac9964e76
Dhrupal Shah <dhrupal.sellergize@gmail.com>
2022-04-03 00:03:05 +0530

    updated config

--- config.php
+++ config.php
@@ -1,7 +1,23 @@
 <?php
 
-define('BASE_URL', 'http://localhost/bill-generator');
-define('BASE_DIR', 'C:/xampp/htdocs/bill-generator');
-define('UPLOAD_FILE_NAME', 'Book1.csv');
+// ENVIRONMENT SPECIFIC CONSTANTS
+if ($_SERVER['SERVER_NAME'] == 'bill-generator-pss.herokuapp.com') {
+    define("ENV", 'production');
+    error_reporting(E_ERROR | E_WARNING | E_PARSE);
+    define('BASE_URL', 'https://bill-generator-pss.herokuapp.com');
+    define('BASE_DIR', "$_SERVER[DOCUMENT_ROOT]");
+} else if ($_SERVER['SERVER_NAME'] != 'localhost') {
+    define("ENV", 'test');
+    error_reporting(E_ALL & ~E_NOTICE);
+    define('BASE_URL', 'http://localhost/bill-generator');
+    define('BASE_DIR', "$_SERVER[DOCUMENT_ROOT]");
+} else {
+    define("ENV", 'dev');
+    error_reporting(E_ALL & ~E_NOTICE);
+    define('BASE_URL', 'http://localhost/bill-generator');
+    define('BASE_DIR', "$_SERVER[DOCUMENT_ROOT]/bill-generator");
+}
+
+define('UPLOAD_FILE_NAME', 'processed.csv');
 define('DEFAULT_TITLE', 'Bill Generator');
 define('DEFAULT_DESCRIPTION', '');
